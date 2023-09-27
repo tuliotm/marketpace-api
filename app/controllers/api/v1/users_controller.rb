5c68dc016926edf07198135f096a3a50666b1be6
tuliotm <tulioteodoromanso@gmail.com>
2023-09-27 20:37:42 -0300

    Adds the user create endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,8 +2,24 @@ module Api::V1
   class UsersController < ApplicationController
     respond_to :json
 
-    def show      
+    def show
       respond_with User.find(params[:id])
+    end
+
+    def create
+      user = User.create(user_params)
+
+      if user.save
+        render json: user, status: :created, localtion: [:api, user]
+      else
+        render json: { errors: user.errors }, status: 422
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
     end
   end
 end
